60163b0291e1993ee5ba7d74a058f84ef67d3ad7
Lebert <lebertbrown38@gmail.com>
2015-07-22 00:09:38 -0400

    Bare Bones of Web App

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Powered by Lebert Brown</title>
+	<link rel="stylesheet" href="Sass/cssOut.css" >
+  <link rel="stylesheet" href="Sass/animate.min.css"> 
+	
+	
+</head>
+<body>
+
+</body>
+</html>
